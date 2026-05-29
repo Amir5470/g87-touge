77bce86a0b715da4d27f8ec427250477810864b8
Amir5470 <amirmechkour5474@gmail.com>
2026-05-28 22:16:33 -0400

    Restart project to move to Godot

--- src/CarModel.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
-
-export function CarModel(props) {
-  const gltf = useGLTF('/assets/models/G87/model.glb');
-  return <primitive object={gltf.scene} {...props} />;
-}
-
-useGLTF.preload('/assets/models/G87/model.glb');
